b862cdd2499c8aee5042722a8a16500350ce00f9
seungwonlee <swlee173@gmail.com>
2023-03-13 15:32:22 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -0,0 +1,23 @@
+import math
+def solution(progresses, speeds):
+    answer = []
+    dateList = []
+    dateValue = 0
+    for i in range(0, len(progresses), 1):
+        dateValue = (100-progresses[i])/speeds[i]
+        dateList.append(math.ceil(dateValue))
+        print(dateList)
+    
+    cnt = 1
+    temp = dateList[0]
+    for i in range(1, len(dateList), 1):
+        print(dateList[i])
+        if temp >= dateList[i]:
+            cnt = cnt + 1
+            
+        else:
+            answer.append(cnt)
+            temp = dateList[i]
+            cnt = 1            
+    answer.append(cnt)
+    return answer
