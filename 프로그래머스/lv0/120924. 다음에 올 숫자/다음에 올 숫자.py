4816c27690023aab4260810b8dd13e84c68fc849
seungwonlee <swlee173@gmail.com>
2023-05-16 09:45:43 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.py
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -1,6 +1,6 @@
 def solution(common):
     answer = 0
-    if common[1] - common[0] == common[2] - common[1]  :
+    if common[1] - common[0] == common[2] - common[1]:
         answer = common[-1] + (common[1] - common[0])
     else:
         answer = common[-1] * (common[1] // common[0])
