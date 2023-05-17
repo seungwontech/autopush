f7df78327f3efd09dcdd39d02b3d3c664f4f292d
seungwonlee <swlee173@gmail.com>
2023-05-17 12:45:14 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -3,9 +3,6 @@ def solution(n):
     str3 = ''
     while n >= 1:
         n, b = divmod(n, 3)
-        print(n, b)
         str3 += str(b)
-        print(str3)
-    print(int(str3, 3))
     answer = int(str3, 3)
     return answer
