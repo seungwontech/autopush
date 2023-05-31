369be701953fcefbff07325bb83f347dc679b610
seungwonlee <swlee173@gmail.com>
2023-05-31 15:48:15 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.py
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.py
@@ -1,7 +1,6 @@
 def solution(n):
     answer = 0
-    templist = list(map(str, str(n)))
-    templist.sort()
+    templist = list(str(n))
     templist.sort(reverse = True)
     answer = int(''.join(templist))
     return answer
