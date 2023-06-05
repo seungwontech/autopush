1d70da1370abd56f63672ebe32e0859b0d6bc85d
seungwonlee <swlee173@gmail.com>
2023-06-05 17:10:25 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -3,5 +3,4 @@ def solution(s):
     s_list = list(map(str, s.lower().split(' ')))
     for i in range(len(s_list)):
         answer += s_list[i].capitalize() + ' '
-        
     return answer[0 : len(answer) - 1]
