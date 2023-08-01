6aa73741513f30ff272f0c726bf7b04c9c56455b
seungwonlee <swlee173@gmail.com>
2023-08-01 16:32:23 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 3.95 ms, Memory: 11 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = []
+    temp = dict()
+    for i in range(len(s)):
+        if s[i] not in temp:
+            answer.append(-1)
+        else:
+            answer.append(i - temp[s[i]])
+        temp[s[i]] = i
+    return answer
