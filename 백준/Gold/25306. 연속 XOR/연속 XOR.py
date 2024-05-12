a63313332f0497c4e24fcc183702eaaf34d1cec4
seungwonlee <swlee173@gmail.com>
2024-05-12 15:30:52 +0900

    [Gold IV] Title: 연속 XOR, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/25306. 연속 XOR/연속 XOR.py
@@ -0,0 +1,12 @@
+A, B = map(int, input().split())
+
+
+def f(n):
+    start = n // 4 * 4
+    answer = 0
+    for i in range(start, n + 1):
+        answer ^= i
+    return answer
+
+
+print(f(A - 1) ^ f(B))
