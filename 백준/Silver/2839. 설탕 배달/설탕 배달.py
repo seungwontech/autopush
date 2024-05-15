917c1dd4a85fb648bc64cc4663c2041b1f7f9b2d
seungwonlee <swlee173@gmail.com>
2024-05-15 10:22:47 +0900

    [Silver IV] Title: 설탕 배달, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,15 @@
+n = int(input())
+bag = 0
+
+if n % 5 == 0:
+    print(n // 5)
+else:
+    while n > 0:
+        n -= 3
+        bag += 1
+        if n == 1 or n == 2:
+            print(-1)
+        if n % 5 == 0:
+            bag += n // 5
+            print(bag)
+            break
