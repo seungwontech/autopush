f85f75b633d410856712f38422b177151a60284e
seungwonlee <swlee173@gmail.com>
2024-05-15 11:49:08 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.02 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int n, int k) {
+        int[] answer = new int[n/k];
+        int index = 0;
+        for (int i = k; i <= n; i+=k) {
+            answer[index] = i;
+            index++;
+        }
+        return answer;
+    }
+}
