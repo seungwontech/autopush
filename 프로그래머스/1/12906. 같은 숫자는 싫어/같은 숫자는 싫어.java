29923d385bb16bbbdee6b68111a5f59e8c603ef3
seungwonlee <swlee173@gmail.com>
2024-05-16 15:00:28 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 51.93 ms, Memory: 117 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        int[] answer = {};
+        Stack<Integer> stack = new Stack<>();
+        for (int num : arr) {
+            if (stack.isEmpty() || stack.peek() != num) {
+                stack.push(num);
+            }
+        }
+        answer = new int[stack.size()];
+        for (int i = answer.length - 1; i >= 0; i--) {
+            answer[i] = stack.pop();
+        }
+
+        return answer;
+    }
+}
