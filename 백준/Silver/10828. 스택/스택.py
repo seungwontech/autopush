eed4470ff265c788cb0960d055031e80550ce432
seungwonlee <swlee173@gmail.com>
2024-05-25 12:35:53 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,27 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+stack = []
+
+for i in range(n):
+    values = sys.stdin.readline().split()
+    if values[0] == 'push':
+        stack.append(values[1])
+    elif values[0] == 'pop':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack.pop())
+    elif values[0] == 'size':
+        print(len(stack))
+    elif values[0] == 'empty':
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+    elif values[0] == 'top':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])
