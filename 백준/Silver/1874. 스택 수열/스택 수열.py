e3815bae61492cb06628bfa505dc3da2be55525d
seungwonlee <swlee173@gmail.com>
2024-05-25 12:51:20 +0900

    [Silver II] Title: 스택 수열, Time: 4044 ms, Memory: 32840 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,23 @@
+n = int(input())
+stack = []
+print_value = []
+k = 1
+isNo = False
+for i in range(n):
+    value = int(input())
+    while k <= value:
+        stack.append(k)
+        print_value.append('+')
+        k += 1
+
+    if stack[-1] == value:
+        stack.pop()
+        print_value.append('-')
+    else:
+        print('NO')
+        isNo = True
+        break
+
+if not isNo:
+    for i in print_value:
+        print(i)
