6e8cd6e60986b0678fe2a906379112876616ed48
seungwonlee <swlee173@gmail.com>
2024-08-22 13:57:01 +0900

    [Bronze V] Title: 개수 세기, Time: 176 ms, Memory: 17860 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] temp = new int[n];
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            temp[i] = a;
+        }
+        int v = sc.nextInt();
+        int cnt = 0;
+        for (int i = 0; i < temp.length; i++) {
+            if (v == temp[i]) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
