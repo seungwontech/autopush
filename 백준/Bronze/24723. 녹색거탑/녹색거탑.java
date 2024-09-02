9be529185b69721e2199edef44487dced030eb10
seungwonlee <swlee173@gmail.com>
2024-09-02 13:09:02 +0900

    [Bronze IV] Title: 녹색거탑, Time: 100 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] a = {1, 2, 4, 8, 16, 32};
+        System.out.println(a[n]);
+    }
+}
