1955962e91f4ef116bed72226c8acbcbf2a8fd16
seungwonlee <swlee173@gmail.com>
2024-09-03 14:25:04 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 164 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int d1 = sc.nextInt();
+        int d2 = sc.nextInt();
+        double pie = 3.141592;
+        System.out.println(d1*2 + 2*pie*d2);
+    }
+}
