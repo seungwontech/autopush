1644f01f33928e0dc8deda49cd4abb20b2493fd7
seungwonlee <swlee173@gmail.com>
2024-10-07 14:58:21 +0900

    [level 1] Title: 기사단원의 무기, Time: 122.02 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            int count = 0;
+            for (int j = 1; j * j<= i; j++) {
+                if(i % j == 0) {
+                    count++;
+                    if (j != i / j) {
+                        count++;
+                    }  
+                }
+                
+            }
+            if(limit < count) {
+                count = power;
+            }
+            answer += count;
+        }
+        return answer;
+    }
+}
