c780a7a813481698f019d10383a88e1c80edf588
seungwonlee <swlee173@gmail.com>
2026-01-21 12:19:37 +0900

    [Bronze II] Title: 분해합, Time: 168 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int result = 0;
+        for (int i = 1; i < N; i++) {
+            int number = i;
+            int sum = i;
+
+            while (number > 0) {
+                sum += number % 10;
+                number /= 10;
+            }
+
+            if (sum == N) {
+                result = i;
+                break;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
